14e4e03a00d1f9a5148679d2aa699aa2d83e887a
jiangs <jshmjs45@sjtu.edu.cn>
2018-11-07 03:36:32 +0900

    submit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# data_for_science
-data for our paper "Chemical Reaction Practicality Judgment via Deep Symbol Artificial Intelligence"
+# 1 data\_for\_science
+Data for our paper "Chemical Reaction Practicality Judgment via Deep Symbol Artificial Intelligence".
+
+ 1. [original_data](https://github.com/jshmjs45/data_for_science/tree/master/original_data)
+ 2. [data\\_for\\_practicality\\_judgment](https://github.com/jshmjs45/data_for_science/tree/master/data_for_practicality_judgment)
+
+All files are encoded in UTF-8 without Byte Order Mark (BOM).
+
+## 1.1 original\_data
+This zipped file includes three files:
+
+1. [data\\_from\\_USPTO\\_utf8](https://github.com/jshmjs45/data_for_science/blob/master/original_data/data_from_USPTO_utf8.tar.gz)
+2. [data\\_from\\_CJHIF\\_utf8](https://github.com/jshmjs45/data_for_science/blob/master/original_data/data_from_CJHIF_utf8.tar.gz)
+3. [data\\_from\\_ChemicalAI\\_Rule\\_utf8](https://github.com/jshmjs45/data_for_science/blob/master/original_data/data_from_ChemicalAI_Rule_utf8.tar.gz)
+4. [data\\_from\\_ChemicalAI\\_Real\\_1\\_utf8](https://github.com/jshmjs45/data_for_science/blob/master/original_data/data_from_ChemicalAI_Real_1_utf8.tar.gz)
+5. [data\\_from\\_ChemicalAI\\_Real\\_2\\_utf8](https://github.com/jshmjs45/data_for_science/blob/master/original_data/data_from_ChemicalAI_Real_2_utf8.tar.gz)
+
+
+### 1.1.1 data\_from\_USPTO_utf8 
+**Positive reactions from USPTO (USPTO)**
+
+This public chemical reaction dataset was extracted from the US patents grants and applications dating from 1976 to September 2016 [US patents grants and applications dating from 1976 to September 2016][1] by [Daniel M. Lowe][2]. The portion of granted patents contains 1,808,938 reactions described using SMILES. Such reaction strings are composed of three groups of molecules: the reactants, the reagents, and the products, which are separated by a ‘>’ sign. After data cleaning with RDKit,an open-source cheminformatics and machine learning tool, it remained 269,132 items at
+last.
+
+The data format is 
+> reactants>reagents>products PatentNumber ParagraphNum Year TextMinedYield CalculatedYield.
+
+[1]: https://figshare.com/articles/Chemical_reactions_from_US_patents_1976-Sep2016_/5104873 "data download link"
+
+[2]: https://www.repository.cam.ac.uk/handle/1810/244727  "D. M. Lowe, Extraction of chemical structures and reactions from the literature, Ph.D. thesis, University of Cambridge (2012)"
+
+### 1.1.2 data\_from\_CJHIF_utf8 
+**Positive reactions from CJHIF(CJHIF)**
+
+3,219,165 reactions mined from high impact factor journals3 with reagent, solvent and
+catalyst information, in addition with yield. After data cleaning and selection, we used
+remaining 1,763,731 items at last. The reaction with expanded information is shown as follows:
+> COC(=O)CCC(Cl)=O>>COC(=O)CCC=O§[CH3:6][C:7]([Cl:8])=[O:9]>>[CH3:6][CH:7]=[O:9]§COC(C)=O;§2,6-dimethylpyridine|hydroge§tetrahydrofuran§palladium on activated charcoal§55
+
+The format of reaction line is
+>reaction§reaction with atom-mapping§functional group 1;§functional group 2;§reagent 1|reagent 2§solvent 1|solvent 2 §catalyst 1|catalyst 2§yield
+
+where >> is the separator to separate the left part (reactant) and the right part (product). Different from USPTO, reaction conditions contain the reagents, solvents and catalysts parts.
+
+###	1.1.3. data\_from\_ChemicalAI\_Rule\_utf8
+**Rule-generated negative reactions from Chemical.AI (Chemical.AI-Rule)**
+
+For every product in the positive reaction sets, we adopt a set of chemical rules to generate
+all possible reactions which may output the respective products. Then we filter the
+resulted reactions by a very large known positive reaction set from Chemical.AI (which
+contains 20 million known reactions collected from chemical literatures and patents).
+Namely, all the remained unreported reactions are taken as negative reactions. Due to
+memory limitation, we keep 100K rule-generated negative reactions in our dataset.
+The data format is 
+>reactants>>products
+
+###	1.1.4. data\_from\_ChemicalAI\_Real\_1\_utf8
+**Real negative reactions from Chemical.AI (Chemical.AI-Real-1)**
+
+12,225 real failed reactions from chemical experiment record of Chemical.AI partner
+laboratories. After data deduplication and canonicalization, it remained 8,797 reactions.
+The data format is 
+>reactants>>products
+
+###	1.1.5. data\_from\_ChemicalAI\_Real\_2\_utf8 
+24,514 real reactions from chemical experiment record of Chemical.AI partner laboratories,
+in which there are 16,137 positive reactions and 8,377 negative reactions, where
+the productivity of negative reactions is 0%. This data set is equally split into two parts:
+training set and test set. The data format is 
+>reactants>>products \t yield \t reagents \t SMILES \t reagents name
+
+
+## 1.2 data\_for\_practicality\_judgment
+### 1.2.1 CJHIF_real1
+
+| Case     | train     | dev     | test    |
+| -------- | --------- | ------- | ------- |
+| Positive | 1,406,259 | 156,251 | 173,624 |
+| Negative | 7,178     | 798     | 874     |
+
+### 1.2.2 USPTO_real1
+### 1.2.3 CJHIF_rule
+### 1.2.4 USPTO_rule
---
+++ codes/class_siamese_final.py
@@ -0,0 +1,271 @@
+from __future__ import print_function
+
+import os
+import sys
+import random
+from time import strftime, gmtime, time
+import pickle
+from keras.layers import Embedding, LSTM, Dense, GRU, Bidirectional, Dropout, Input, Lambda, concatenate
+from keras.models import Sequential, Model
+from keras.metrics import mae
+from keras.optimizers import RMSprop, SGD
+
+random.seed(233)
+import numpy as np
+import argparse
+import logging
+from datetime import datetime
+from keras import backend as K
+from keras.layers.core import Dense, Dropout, Activation
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--maxlen', dest='maxlen', type=int, default=100)
+parser.add_argument('--layer', dest='layer', type=str, default="biLSTM")
+parser.add_argument('--rateid', dest='rateid', type=int, default=0)
+parser.add_argument('--dim', dest='dim', type=int, default=64)
+parser.add_argument('--epochs', dest='epochs', type=int, default=50)
+parser.add_argument('--mode', dest='mode', type=int, default=0)
+parser.add_argument('--folder', dest='folder', type=str, default="CJHIF-rule")
+parser.add_argument('--data', dest='data', type=str, default="/home/zhangzs/chemistry/USPTO-rule")
+
+args = parser.parse_args()
+params = vars(args)
+print(params)
+path = params['data']
+mode = params['mode']
+rate_id = params['rateid']
+maxlength = params['maxlen']
+layer = params['layer']
+dim = params['dim']
+epochs = params['epochs']
+folder = params['folder']
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename=datetime.now().strftime('logs/mylogfile_%H_%M_%d_%m_%Y' + "_" + folder + ".log"),
+                    filemode='w')
+
+logging.info(str(params))
+
+def log(x):
+    print(x)
+
+
+def load(path, name):
+    print("path:", os.path.join(path, name))
+    return pickle.load(open(os.path.join(path, name), 'rb'))
+
+
+def padq(data):
+    return pad(data, maxlength)
+
+
+def pada(data):
+    return pad(data, maxlength)
+
+
+def pad(data, len=None):
+    from keras.preprocessing.sequence import pad_sequences
+    return pad_sequences(data, maxlen=len, padding='post', truncating='post', value=0)
+
+
+def get_time():
+    return strftime('%Y-%m-%d %H:%M:%S', gmtime())
+
+
+def output(predictions):
+    out = []
+    predictions = predictions.tolist()
+    for i, d in enumerate(predictions):
+        if d[0] > 1:
+            out.append(0)
+        else:
+            out.append(1 - d[0])
+    return out
+
+
+vocab = load(path, 'vocabulary')
+vocab_size = len(vocab) + 1
+
+
+def save_epoch(epoch):
+    foldername = "mse/" + str(rate_id)
+    if folder != "":
+        foldername += "/" + folder
+    if not os.path.exists('models/' + foldername):
+        os.makedirs('models/' + foldername)
+    model.save_weights('models/' + foldername + '/weights_epoch_%d.h5' % epoch, overwrite=True)
+
+
+def process(dataset):
+    reactants = load(dataset, 'question')
+    products = load(dataset, 'answer')
+    steps = load(dataset, 'step')
+    labels = load(dataset, 'label')
+    new_reactants = []
+    new_products = []
+    new_steps = []
+    new_labels = []
+    pos_reactants = []
+    pos_products = []
+    pos_steps = []
+    pos_labels = []
+    neg_reactants = []
+    neg_products = []
+    neg_steps = []
+    neg_labels = []
+
+    new_reactants = reactants
+    new_products = products
+    new_steps = steps
+    new_labels = labels
+    
+    print("reactants:" + str(len(reactants)))
+    print("products:" + str(len(products)))
+    print("steps:" + str(len(steps)))
+    print("labels:" + str(len(labels)))
+
+    logging.info("reactants:" + str(len(reactants)))
+    logging.info("products:" + str(len(products)))
+    logging.info("steps:" + str(len(steps)))
+    logging.info("labels:" + str(len(labels)))
+
+    reactants = padq(reactants)
+    products = pada(products)
+    steps = pada(new_steps)
+    y = pad(new_labels, len=1)
+
+    pos = np.sum(y == 1, axis=0)
+    neg = np.sum(y == 0, axis=0)
+    print("pos:" + str(pos))
+    print("neg:" + str(neg))
+    logging.info("pos:" + str(pos))
+    logging.info("neg:" + str(neg))
+
+    if mode == 0:
+        r = np.concatenate((reactants, steps), axis=1)
+        p = np.concatenate((products, steps), axis=1)
+        print("r/p + step")
+    else:
+        r = reactants
+        p = products
+        print("r/p")
+    print("cocat:" + str(len(r)))
+    return r, p, y
+
+
+r, p, y = process(path + "/train/")
+tr, tp, ty = process(path + "/test/")
+
+print('Build model...')
+input_dim = r.shape[1]
+
+
+def create_base_network(input_dim):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    print('Build model...')
+
+    model = Sequential()
+    embedding = Embedding(input_dim=vocab_size,
+                          output_dim=128,
+                          mask_zero=True)
+    model.add(embedding)
+    model.add(LSTM(256))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    return model
+
+
+base_network = create_base_network(input_dim)
+input_a = Input(shape=(input_dim,))
+input_b = Input(shape=(input_dim,))
+
+processed_a = base_network(input_a)
+processed_b = base_network(input_b)
+
+joint = concatenate([processed_a, processed_b], axis=1)
+joint_den = Dense(256, init='normal', activation='relu')(joint)
+distance = Dense(1, init='normal', activation='sigmoid')(joint_den)
+
+model = Model([input_a, input_b], distance)
+
+
+
+def compute_accuracy(predictions, labels, threshod):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    TP, TN, predict_true, predict_false = 0., 0., 0., 0.
+    actual = np.sum(labels == 1, axis=0)
+    for i, d in enumerate(predictions):
+        if d >= threshod:
+            predict_true += 1
+            if labels[i] == 1:
+                TP += 1
+        else:
+            predict_false += 1
+            if labels[i] == 0:
+                TN += 1
+
+    precision = TP / float(predict_true)
+    acc =  (TP+TN) / float(len(labels))
+    recall = TP / float(actual)
+    fscore = (2 * precision * recall) / float(precision + recall)
+    return precision, recall, fscore, acc
+
+
+def compute_accuracy_neg(predictions, labels, threshold):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    right, predict = 0, 0
+    actual = np.sum(labels == 0, axis=0)
+    for i, d in enumerate(predictions):
+        if d < threshold:
+            predict += 1
+            if labels[i] == 0:
+                right += 1
+    print("neg evaluation")
+    print("all: " + str(len(labels)))
+    print("actual: " + str(actual[0]))
+    print("predict: " + str(predict))
+    print("right: " + str(right))
+    if not predict == 0:
+        precision = right / float(predict)
+    else:
+        precision = 0
+    if not actual == 0:
+        recall = right / float(actual)
+    else:
+        recall = 0
+    if predict == 0 or actual == 0:
+        fscore = 0
+    else:
+        fscore = (2 * precision * recall) / float(precision + recall)
+    return precision, recall, fscore
+
+rms = RMSprop()
+model.compile(loss='binary_crossentropy', optimizer=rms, metrics=['accuracy'])
+val_loss = {'loss': 1., 'epoch': 0}
+for i in range(1, epochs):
+        log('%s -- Epoch %d ' % (get_time(), i))
+        logging.info('%s -- Epoch %d ' % (get_time(), i))
+        hist = model.fit([r, p], y, batch_size=512, shuffle=True, epochs=1)
+        pred = model.predict([tr, tp], batch_size=512)
+        te_acc, te_rr, te_f, acc = compute_accuracy(pred, ty, 0.5)
+        n_acc, n_rr, n_f = compute_accuracy_neg(pred, ty, 0.5)
+        print('* accuracy on test set: %0.2f%%' % (100 * acc))
+        logging.info('* accuracy on test set: %0.2f%%' % (100 * acc))
+        print('* precision on test set: %0.2f%%' % (100 * te_acc))
+        logging.info('* acc on test set: %0.2f%%' % (100 * te_acc))
+        print('* recall on test set: %0.2f%%' % (100 * te_rr))
+        logging.info('* recall on test set: %0.2f%%' % (100 * te_rr))
+        print('* Fscore on test set: %0.2f%%' % (100 * te_f))
+        logging.info('* Fscore on test set: %0.2f%%' % (100 * te_f))
+        print('* neg precision: %0.2f%%' % (100 * n_acc))
+        logging.info('* Fscore on neg test set: %0.2f%%' % (100 * n_acc))
+        print('* neg recall: %0.2f%%' % (100 * n_rr))
+        logging.info('* recall on neg test set: %0.2f%%' % (100 * n_rr))
+        print('* neg Fscore: %0.2f%%' % (100 * n_f))
+        logging.info('* Fscore on neg test set: %0.2f%%' % (100 * n_f))
+        save_epoch(i)
